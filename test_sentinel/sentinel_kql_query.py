f97e0e954d3ebeb7d17b24fe339a6da218359729
Anekant Jain <anekant.employee@gmail.com>
2025-10-30 16:49:46 +0530

    Till now working

--- test_sentinel/sentinel_kql_query.py
+++ test_sentinel/sentinel_kql_query.py
@@ -1,25 +1,23 @@
 import os
 import requests
-from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential
 
 # Load environment variables if needed
 workspace_id = "674f96b1-63d6-48ca-9fe6-d613e4292c7f"  # Log Analytics Workspace ID
 
 kql_query = """
-SecurityEvent
+SigninLogs
 | where TimeGenerated > ago(1d)
 | take 10
 """
 
 credential = DefaultAzureCredential()
+
 token = credential.get_token("https://api.loganalytics.io/.default").token
-
 url = f"https://api.loganalytics.io/v1/workspaces/{workspace_id}/query"
-
 headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-
-body = {"query": kql_query, "timespan": "P1DT3H"}
+body = {"query": kql_query, "timespan": "P1D"}
 
 response = requests.post(url, headers=headers, json=body)
 
